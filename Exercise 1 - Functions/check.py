4d5d83feee33363bf9ffbd89e8dca11f86303dbe
Uzair Tariq <uzair0213@icloud.com>
2019-03-05 10:05:21 -0700

    Add interactivity

--- Exercise 1 - Functions/check.py
+++ Exercise 1 - Functions/check.py
@@ -3,3 +3,12 @@ def check(number, number_range):
         return True
     else:
         return False
+
+
+number = int(input("Enter the number you want to check for: "))
+num_range = int(input("Enter the range to check for the number (ex. 10): "))
+checked = check(number, num_range)
+if checked == True:
+    print("The number {} is in the range of {}".format(number, num_range))
+else:
+    print("The number {} is not in the range of {}".format(number, num_range))

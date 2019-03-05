0935e2d892a3a59d68aac857e6a2263a5f31024c
Uzair Tariq <uzair0213@icloud.com>
2019-03-05 10:11:49 -0700

    Simplify a statement

--- Exercise 1 - Functions/check.py
+++ Exercise 1 - Functions/check.py
@@ -8,7 +8,7 @@ def check(number, number_range):
 number = int(input("Enter the number you want to check for: "))
 num_range = int(input("Enter the range to check for the number (ex. 10): "))
 checked = check(number, num_range)
-if checked == True:
+if checked:
     print("The number {} is in the range of {}".format(number, num_range))
 else:
     print("The number {} is not in the range of {}".format(number, num_range))

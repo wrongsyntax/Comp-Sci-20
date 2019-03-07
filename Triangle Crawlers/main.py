baa7e1e2f8c5ab8267f6c5d7302cab26707d7c7a
Uzair Tariq <uzair0213@icloud.com>
2019-03-07 11:23:19 -0700

    Redo everything
    Recode everything and calculate the total average

--- Triangle Crawlers/main.py
+++ Triangle Crawlers/main.py
@@ -1,39 +1,43 @@
 import random
 
 possible_pos = ["A", "B", "C", "D"]
-sim_count = 100
+original_sim_count = int(input("How many crawlers would you like to simulate? "))
+sim_count = original_sim_count
+averages = []
 
 
 def run_sim():
-    crawler_pos = "A"                                           # Sets initial location of crawler
-    print(crawler_pos)
+    crawler_pos = possible_pos[0]
     last_pos = crawler_pos
     age = 1
-    sum_of_ages = 1
+    age_list = [1]
+
     while crawler_pos != possible_pos[3]:
         new_pos = random.randint(0, 3)
-        if (crawler_pos != possible_pos[3]                      # Makes sure the crawler is allowed
-                and possible_pos[new_pos] != crawler_pos        # to move to the new position
-                and possible_pos[new_pos] != last_pos):
+        if crawler_pos != new_pos and crawler_pos != last_pos:
             last_pos = crawler_pos
             crawler_pos = possible_pos[new_pos]
-            print(crawler_pos)
             age += 1
-            sum_of_ages = age + sum_of_ages
-            average = sum_of_ages / age
-        elif crawler_pos == last_pos:                           # Makes sure that the crawler doesn't
-            new_pos = random.randint(0, 3)                      # move back to where it came from
-            if (crawler_pos != possible_pos[3]
-                    and possible_pos[new_pos] != crawler_pos
-                    and possible_pos[new_pos] != last_pos):
-                last_pos = crawler_pos
-                crawler_pos = possible_pos[new_pos]
-                print(crawler_pos)
-                age += 1
-                sum_of_ages = age + sum_of_ages
-                average = sum_of_ages / age
+            age_list.append(age)
+        elif crawler_pos == last_pos:
+            last_pos = crawler_pos
+            crawler_pos = possible_pos[new_pos]
+            age += 1
+            age_list.append(age)
+
+    average = sum(age_list) / len(age_list)
+    return average
 
 
-for sims in range(sim_count):                                   # Runs the simulation
-    run_sim()
-    print("\n")
+print("Running simulation...")
+
+while sim_count != 0:
+    averages.append(run_sim())
+    sim_count -= 1
+
+print("Simulation complete.")
+total_average = sum(averages) / len(averages)
+youngest = min(averages)
+oldest = max(averages)
+print("The total average lifespan of {} crawlers is {} days.".format(original_sim_count, total_average))
+print("The youngest crawler to die was {} days old. The oldest to die was {} days old.".format(youngest, oldest))

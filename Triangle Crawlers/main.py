2388376f99c2072ab36d1fe5a0ce322e1ab32d7d
Uzair Tariq <uzair0213@icloud.com>
2019-03-06 19:25:33 -0700

    Add average calculations

--- Triangle Crawlers/main.py
+++ Triangle Crawlers/main.py
@@ -8,7 +8,8 @@ def run_sim():
     crawler_pos = "A"                                           # Sets initial location of crawler
     print(crawler_pos)
     last_pos = crawler_pos
-    # TODO: Math part
+    age = 1
+    sum_of_ages = 1
     while crawler_pos != possible_pos[3]:
         new_pos = random.randint(0, 3)
         if (crawler_pos != possible_pos[3]                      # Makes sure the crawler is allowed
@@ -17,6 +18,9 @@ def run_sim():
             last_pos = crawler_pos
             crawler_pos = possible_pos[new_pos]
             print(crawler_pos)
+            age += 1
+            sum_of_ages = age + sum_of_ages
+            average = sum_of_ages / age
         elif crawler_pos == last_pos:                           # Makes sure that the crawler doesn't
             new_pos = random.randint(0, 3)                      # move back to where it came from
             if (crawler_pos != possible_pos[3]
@@ -25,8 +29,11 @@ def run_sim():
                 last_pos = crawler_pos
                 crawler_pos = possible_pos[new_pos]
                 print(crawler_pos)
+                age += 1
+                sum_of_ages = age + sum_of_ages
+                average = sum_of_ages / age
 
 
-for i in range(sim_count):                                      # Runs the simulation
+for sims in range(sim_count):                                   # Runs the simulation
     run_sim()
     print("\n")

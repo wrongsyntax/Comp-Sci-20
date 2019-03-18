34cdefc9964ce9cdcb439dc4d5f73a5d5487e6d3
Uzair Tariq <uzair0213@icloud.com>
2019-03-18 10:38:21 -0600

    Add code

---
+++ Exercise 3 - More Functions/pascal.py
@@ -0,0 +1,17 @@
+n = int(input("How many rows would you like to print? "))
+
+
+def generate_pascal_triangle(rows):
+    if rows == 1: return [[1]]
+    triangle = [[1], [1, 1]]
+    row = [1, 1]
+    for i in range(2, rows):
+        row = [1] + [sum(column) for column in zip(row[1:], row)] + [1]
+        triangle.append(row)
+
+    return triangle
+
+
+for row in generate_pascal_triangle(n):
+    print(row)
+

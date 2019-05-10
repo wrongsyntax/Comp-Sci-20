7bf333f32f5f6f9a397e5f8f07e2dd33b5e93845
Uzair Tariq <uzair0213@icloud.com>
2019-05-10 09:24:30 -0600

    Start work on function

---
+++ Exercise 1 - Data Structures/words.py
@@ -0,0 +1,8 @@
+words = ['Roses', 'are', 'red', 'Violets', 'are', 'blue',
+         'Computer', 'Science', 'is', 'fun', 'Python', 'is', 'too']
+
+number = input("Enter a number: ")
+
+
+def find_words(number, words):
+    pass

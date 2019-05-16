d2ba7da7cd2581248e0c3dabaed17cc6a680f383
Uzair Tariq <uzair0213@icloud.com>
2019-05-16 10:44:56 -0600

    Get function working

--- Exercise 1 - Data Structures/words.py
+++ Exercise 1 - Data Structures/words.py
@@ -1,8 +1,20 @@
 words = ['Roses', 'are', 'red', 'Violets', 'are', 'blue',
          'Computer', 'Science', 'is', 'fun', 'Python', 'is', 'too']
 
-number = input("Enter a number: ")
+number = int(input("Enter a number: "))
 
 
-def find_words(number, words):
-    pass
+def find_words(num, word_list):
+    found = []
+    for word in word_list:
+        if len(word) >= num:
+            found.append(word)
+        else:
+            pass
+
+    return found
+
+
+new_words = find_words(number, words)
+
+print("The words that are longer than {} are {}.".format(number, new_words))

f8f44806ab0d7841f1be0f416543cf68a20dc359
Uzair Tariq <uzair0213@icloud.com>
2019-06-11 17:28:51 -0600

    Small changes

--- Assignment 3 - Battleship/main.py
+++ Assignment 3 - Battleship/main.py
@@ -8,19 +8,18 @@ for i in range(7):
 
 def print_board(board_in):
     for row in range(len(board_in)):
-        print(" ".join(board_in[row]))
+        print("  ".join(board_in[row]))
 
 
 def random_row(board_in):
-  return randint(0, len(board_in)-1)
+    return randint(0, len(board_in)-1)
 
 
 def random_col(board_in):
-  return randint(0, len(board_in)-1)
+    return randint(0, len(board_in)-1)
 
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-
 
 print_board(board)
